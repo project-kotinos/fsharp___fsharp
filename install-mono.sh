307310a1c96a7a9c64ac979392f40c42e7914811
John Ewart <john@johnewart.net>
2019-07-22 16:34:56 -0700

    Update install-mono.sh

--- install-mono.sh
+++ install-mono.sh
@@ -4,4 +4,4 @@ apt -y install gnupg ca-certificates
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
 echo "deb https://download.mono-project.com/repo/ubuntu stable-bionic main" > /etc/apt/sources.list.d/mono-official-stable.list
 apt update
-apt -y install mono-devel
+apt -y install mono-devel sudo
