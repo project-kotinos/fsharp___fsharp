5fb7c1e247749c5e56bf296661c4049bba2a471c
John Ewart <john@johnewart.net>
2019-07-22 19:50:35 -0700

    Update prepare-mono.sh

--- mono/prepare-mono.sh
+++ mono/prepare-mono.sh
@@ -19,12 +19,12 @@ case $OSName in
 esac
 
 # On Linux (or at least, Ubuntu), when building with Mono, need to install the mono-devel package first.
-if [ $OS = 'Linux' ]; then
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
-    echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
-    sudo apt-get update
-    sudo apt-get -y install mono-devel msbuild
-fi
+#if [ $OS = 'Linux' ]; then
+#    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 3FA7E0328081BFF6A14DA29AA6A19B38D3D831EF
+#    echo "deb http://download.mono-project.com/repo/ubuntu trusty main" | sudo tee /etc/apt/sources.list.d/mono-xamarin.list
+#    sudo apt-get update
+#    sudo apt-get -y install mono-devel msbuild
+#fi
 
 # The FSharp.Compiler.Tools package doesn't work correctly unless a proper install of F# has been done on the machine.
 # OSX can skip this because the OSX Mono installer includes F#.
